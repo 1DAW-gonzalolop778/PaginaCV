3bfa73a43ab4c81d9292f72cae2b70d964bd4f43
1DAW-gonzalolop778 <glopluq0208@g.educaand.es>
2025-10-27 13:12:39 +0100

    m

---
+++ app.js
@@ -0,0 +1,11 @@
+// app.js
+const scroll = new LocomotiveScroll({
+  el: document.querySelector('#js-scroll'),
+  smooth: true,
+  smartphone: {
+    smooth: true
+  },
+  tablet: {
+    smooth: true
+  }
+});
